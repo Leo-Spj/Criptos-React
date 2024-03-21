347a614c4669f4e1dad0685ddf3fc3b12fde4666
Leonardo Espejo <85245534+Leo-Spj@users.noreply.github.com>
2024-03-21 15:30:39 -0500

    bienvenida - home

---
+++ src/Home.jsx
@@ -0,0 +1,14 @@
+import './Home.css';
+import { Link } from 'react-router-dom'
+
+function Home() {
+    return ( 
+        <div className="home-container">
+        <h1 className="title">Bienvenido a CriptoApp</h1>
+        <p className="subtitle">Cónoce las 100 criptos más usadas</p>
+        <Link to="/criptomonedas" className="link">Ver criptomonedas</Link>
+        </div>
+     );
+}
+
+export default Home;
