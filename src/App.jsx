8925651aa50b8206eb0a63574aa7cca0c7a04a16
Leonardo Espejo <85245534+Leo-Spj@users.noreply.github.com>
2024-03-20 23:58:31 -0500

    API, Variables de entorno y List Keys

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,31 @@
+import React, { useState, useEffect } from 'react';
 
 function App() {
 
+  const [criptos, setCriptos] = useState();
+  
+  const API_URL = import.meta.env.VITE_API_URL;
+  
+  useEffect(() => {
+    fetch(`${API_URL}assets`)
+      .then(response => response.json())
+      .then(data => setCriptos(data.data))
+      .catch(error => console.log(error))
+  }, []);
+
+  if (!criptos) { return <h1>Cargando...</h1> }
+
   return (
     <>
-      
+      <h1>Lista de criptomonedas</h1>
+
+      <ol>
+        {
+          criptos.map( cripto =>(
+            <li key={cripto.id}>{cripto.name} - Precio: {cripto.priceUsd} </li>          
+          ))
+        }
+      </ol>
     </>
   )
 }
