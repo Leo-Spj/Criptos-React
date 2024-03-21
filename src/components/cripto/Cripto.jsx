924002f4c7c609db73b0836813e4f6b7214f6362
Leonardo Espejo <85245534+Leo-Spj@users.noreply.github.com>
2024-03-21 15:33:24 -0500

    Agregada navegación a detalle de criptomonedas en Cripto.jsx

--- src/components/cripto/Cripto.jsx
+++ src/components/cripto/Cripto.jsx
@@ -1,21 +1,26 @@
 import './Cripto.css';
 
+import { Link } from 'react-router-dom'
+
 // 'ffc' snippet
-function Cripto({name, priceUsd, symbol, changePercent24Hr}) {
+function Cripto({id, name, priceUsd, symbol, changePercent24Hr}) {
     return ( 
-        <div className='cripto'>
-            <h2>{name}</h2>
-            <div className="info">
-                <p><span className="label">Precio: </span>{parseFloat(priceUsd).toFixed(5)}</p>
-                <p><span className="label">Código: </span>{symbol}</p>
-                <p>
-                    <span className="label">Variación 24h: </span>
-                    <span className={changePercent24Hr > 0 ? 'positivo' : 'negativo'}>
-                        {parseFloat(changePercent24Hr).toFixed(3)}%
-                    </span>
-                </p>
+        <Link to={`/criptomonedas/${id}`}>
+            <div className='cripto'>
+                <h2>{name}</h2>
+                <div className="info">
+                    <p><span className="label">Precio: </span>{parseFloat(priceUsd).toFixed(5)}</p>
+                    <p><span className="label">Código: </span>{symbol}</p>
+                    <p>
+                        <span className="label">Variación 24h: </span>
+                        <span className={changePercent24Hr > 0 ? 'positivo' : 'negativo'}>
+                            {parseFloat(changePercent24Hr).toFixed(3)}%
+                        </span>
+                    </p>
+
+                </div>
             </div>
-        </div>
+        </Link>
      );
 }
 
