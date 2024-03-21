6bbd2fcb237373981173ef890dc67d6debcb9d60
Leonardo Espejo <85245534+Leo-Spj@users.noreply.github.com>
2024-03-21 15:27:21 -0500

    404

---
+++ src/components/404.jsx
@@ -0,0 +1,16 @@
+import './404.css';
+import { NavLink } from 'react-router-dom'
+
+function Pagina404() {
+    return ( 
+        <div className='no-404'>
+            <h1>Página no encontrada</h1>
+            <span>La página solicitada, no existe</span>
+            <span>
+                <NavLink to="/">Volver</NavLink>
+            </span>
+        </div>
+     );
+}
+
+export default Pagina404;
