38a88f691dac5de63afde2f3aab7913a08077ed8
Leonardo Espejo <85245534+Leo-Spj@users.noreply.github.com>
2024-03-22 14:01:08 -0500

    Actualizar vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:"/criptos-react/"
+  //base:"/criptos-react/"
   //base:"https://leo-spj.github.io/criptos-react",
 })
