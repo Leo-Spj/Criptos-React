446d116f65c3c433416138bb3da7e602724dba02
Leonardo Espejo <85245534+Leo-Spj@users.noreply.github.com>
2024-03-22 00:29:17 -0500

    Actualiza enrutador y rutas en main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -8,19 +8,19 @@ import Home from './Home.jsx'
 import Cuadricula from './components/Cuadricula.jsx'
 import Pagina404 from './components/404.jsx'
 
-import { HashRouter as Router } from 'react-router-dom' // 'imbr' snippet
-import { Route } from 'react-router-dom' // 'imrr' snippet
+import { BrowserRouter as Router } from 'react-router-dom' // 'imbr' snippet
+import { Routes, Route } from 'react-router-dom' // 'imrr' snippet
 import CriptoPage from './components/cripto/CriptoPage.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 
  <Router>
 
-
+    <Routes>
 
       <Route path="*" element={<Pagina404 />} />
 
-      <Route path="/" element={<App />}>
+      <Route path="/criptos-react/" element={<App />}>
           <Route index element={<Home />} />
       </Route>  
 
@@ -29,7 +29,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
           <Route path=":id" element={<CriptoPage/>} />
       </Route>  
 
-
+    </Routes>
 
  </Router>
 
