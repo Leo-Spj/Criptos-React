34c42c4e60d8716cbf34569ac14bd540ce51b5d7
Leonardo Espejo <85245534+Leo-Spj@users.noreply.github.com>
2024-03-21 20:42:14 -0500

    Añadir estilos y componentes para la página de cripto

--- src/components/cripto/CriptoPage.jsx
+++ src/components/cripto/CriptoPage.jsx
@@ -1,10 +1,11 @@
 import './CriptoPage.css';
+
 import usePetition from '../hooks/usePetition';
 
-import { useParams } from 'react-router-dom';
+import CriptoInfo from './info/CriptoInfo';
+import CriptoHistorial from './info/CriptoHistorial';
 
-import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from 'recharts';
-import { format } from 'date-fns';
+import { useParams } from 'react-router-dom';
 
 
 
@@ -15,78 +16,23 @@ function CriptoPage() {
     const cryptoData = usePetition(`assets/${param.id}`);
     const history = usePetition(`assets/${param.id}/history?interval=d1`);
 
-
-
-    if (!cryptoData || !history) { 
-        return(
-           <h1 className='app-container'>Cargando...</h1> 
-        )
+    if(!cryptoData || !history) {
+        return (
+            <div className='cripto-page-container'>
+                <h1>Cargando...</h1>
+            </div>
+        );
     }
 
-
-    const width = Math.max(window.innerWidth * 0.9, 300);
-
     return (
-    <>
-
+        <>
         <div className='cripto-page-container'>
-
-            {
-                (cryptoData) && (
-                    <div className='info'>
-
-                        <div className='main-info'>
-                            <span>Ranking: {cryptoData.rank}</span>
-                            <h1>{cryptoData.name}</h1>
-                            <span className='symbol'>{cryptoData.symbol}</span>
-                        </div>
-
-                        <div className='details'>
-                            <ul>
-                                {cryptoData && Object.entries(cryptoData).map(([key, value]) => {
-                                    if (key !== 'id' && key !== 'rank' && key !== 'symbol' && key !== 'name') {
-                                        return (
-                                            <li key={key}>
-                                                <span className='label'>{key}: </span>
-                                                <span>{value}</span>
-                                            </li>
-                                        );
-                                    }
-                                    <li key={key}></li>
-                                })}
-                            </ul>
-                        </div>
-                    </div>
-                )
-            }
-        
-            
-
-            {
-                (history) && (
-                    <div className=''>
-                        <h2>{`Historia`}</h2>
-                        <BarChart width={width} height={200} data={history}>
-                            <CartesianGrid strokeDasharray='5 5' />
-                            <XAxis 
-                                dataKey='date' 
-                                tickFormatter={(str) => {
-                                const date = new Date(str);
-                                return format(date, 'dd/MM');
-                                }}
-                            />
-                            <YAxis />
-                            <Tooltip/>
-                            <Legend />
-                            <Bar dataKey='priceUsd' name='Precio (USD)' fill='#8884d8'/>
-                        </BarChart>
-                    </div>
-                )
-                
-            }
-
+            {(cryptoData) && (<CriptoInfo cryptoData={cryptoData}/>)}
         </div>
-    </>
+        <div className='grafica-container'>
+            {(history) && <CriptoHistorial history={history} />}
+        </div>
+        </>
     );
 }
 
