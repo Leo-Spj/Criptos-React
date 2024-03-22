f0feab3a65fba2fbeaac58b9454dee7ce47d3fb0
Leonardo Espejo <85245534+Leo-Spj@users.noreply.github.com>
2024-03-22 13:20:18 -0500

    Actualizar base URL en vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:"/"
+  base:"/criptos-react/"
   //base:"https://leo-spj.github.io/criptos-react",
 })
