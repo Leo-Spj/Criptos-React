fed56a0cca9ca7da2bbd26d00ee7d04ef3cf86eb
Leonardo Espejo <85245534+Leo-Spj@users.noreply.github.com>
2024-03-23 10:46:19 -0500

    Actualizar estilos de inicio y corregir error ortográfico en el texto de bienvenida

--- src/Home.jsx
+++ src/Home.jsx
@@ -5,7 +5,7 @@ function Home() {
     return ( 
         <div className="home-container">
         <h1 className="title">Bienvenido a CriptoApp</h1>
-        <p className="subtitle">Cónoce las 100 criptos más usadas</p>
+        <p className="subtitle">Conoce las 100 criptos más usadas</p>
         <Link to="/criptomonedas" className="link">Ver criptomonedas</Link>
         </div>
      );
